f72b67eb7207b0e3dade94787fded62a14ef69de
MohammadFaizan74 <mdf18083@gmail.com>
2024-03-01 16:29:25 +0530

    api calling using fetch

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import React from 'react'
+
+
+
+
+const App = () => {
+  
+ const [data,setData]=useState('')
+ const[count,setCount]=useState(0)
+
+
+ const onBtnClick=()=>{
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(res =>setData(res))
+ }
+ useEffect(()=>{
+  
+ },[])
+
+const onCountClick=()=>{
+  setCount(count=>count+1)
+}
+
+
+  return (
+    <div>
+   <h1>App</h1>
+   <h1>Counter-{count}</h1>
+   <h2>{JSON.stringify(data)}</h2>
+   <button onClick={()=>onBtnClick()}>Test</button>
+   <button onClick={()=>onCountClick()}>Counter</button>
+
+
+    </div>
+  )
+}
+
+export default App
